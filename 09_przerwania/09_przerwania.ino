5079dc20a83f5e645ade5a46669443ea57a79872
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-04-13 18:46:15 +0200

    Kod programu do lekcji 9

---
+++ 09_przerwania/09_przerwania.ino
@@ -0,0 +1,34 @@
+#define LED_R 10
+#define LED_G 11
+#define LED_B 12
+#define LED_SYG 13
+
+#define KONTAKTRON 0
+#define PIR 2
+
+void setup() {
+  pinMode(LED_R, OUTPUT); //Poszczególne piny sterujące diodą jako wyjścia
+  pinMode(LED_G, OUTPUT);
+  pinMode(LED_B, OUTPUT);
+
+  pinMode(KONTAKTRON, INPUT_PULLUP); //Kontaktron joko wejście
+  pinMode(PIR, INPUT); //PIR jako wejście
+
+  digitalWrite(LED_R, LOW);
+  digitalWrite(LED_G, HIGH);
+  digitalWrite(LED_B, LOW);
+
+  attachInterrupt(digitalPinToInterrupt(PIR), alarm, RISING); //Przerwanie reagujące na zbocze
+}
+
+void loop() {
+  digitalWrite(LED_SYG, HIGH);
+  delay(2000);
+  digitalWrite(LED_SYG, LOW);
+  delay(2000);
+}
+
+void alarm() {
+  digitalWrite(LED_R, HIGH); //Stan ALARM - dioda czerwona
+  digitalWrite(LED_G, LOW);
+}
