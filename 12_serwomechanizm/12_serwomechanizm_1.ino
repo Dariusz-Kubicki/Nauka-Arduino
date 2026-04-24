254653cecf5cc5cfa23ef2c9b14e81c5ae660ada
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-04-24 14:59:27 +0200

    Update 12_serwomechanizm_1.ino

--- 12_serwomechanizm/12_serwomechanizm_1.ino
+++ 12_serwomechanizm/12_serwomechanizm_1.ino
@@ -1,20 +1,20 @@
-#include <Servo.h>
+#include <Servo.h> //Biblioteka odpowiedzialna za serwa
 
-Servo serwomechanizm;
-int pozycja = 180;
-int zmiana = 6;
+Servo serwomechanizm; //Tworzenie obiektu, dzięki któremu możemy odwołać się do serwa
+int pozycja = 180; //Atualna pozycja serwa (u mnie serwo działa w przeciwną stronę do wskazówek zegara)
+int zmiana = 6; //Co ile ma się zmieniać pozycja serwa
 
 void setup() {
-  serwomechanizm.attach(9);
+  serwomechanizm.attach(9); //Serwomechanizm podłączony do pinu 9
 }
 
 void loop() {
-  if (pozycja > 0) {
-    serwomechanizm.write(pozycja);
-  } else {
+  if (pozycja > 0) { //Jeśli pozycja mieści się w zakresie
+    serwomechanizm.write(pozycja); //Wykonaj ruch
+  } else { //Jeśli nie, to powrót na początek
     pozycja = 180;
   }
 
-  pozycja = pozycja - zmiana;
-  delay(200);
+  pozycja = pozycja - zmiana; //Zwiększenie aktualnej pozycji serwa
+  delay(200); //Opóźnienie dla lepszego efektu
 }
