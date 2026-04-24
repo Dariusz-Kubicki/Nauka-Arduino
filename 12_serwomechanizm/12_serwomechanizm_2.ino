4aaabd565f0f0e323dee212fb0f02cb66152695c
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-04-24 14:36:04 +0200

    Update 12_serwomechanizm_2.ino

--- 12_serwomechanizm/12_serwomechanizm_2.ino
+++ 12_serwomechanizm/12_serwomechanizm_2.ino
@@ -1,8 +1,6 @@
 #include <Servo.h> //Biblioteka odpowiedzialna za serwa
 #define pinServo 9
 Servo serwomechanizm; //Tworzenie obiektu dzięki któremu można się odwołać do serwa
-String odebraneDane = ""; //Odebrane dane String z UART
-int daneInt = 0; //Przekształcone odebrane dane String z UART na dane int
 
 void setup() {
   serwomechanizm.attach(pinServo); //Podłączenie serwomechanizmu do pinu pinServo
@@ -11,6 +9,8 @@ void setup() {
 }
 
 void loop() {
+  String odebraneDane = ""; //Odebrane dane String z UART
+  int daneInt = 0; //Przekształcone odebrane dane String z UART na dane int
   if (Serial.available() > 0) { //Czy Arduino odebrało dane
     odebraneDane = Serial.readStringUntil('\n'); //Jeśli tak to zapisz odebrane dane do zmiennej
     daneInt = odebraneDane.toInt(); // i zamień te dane na zmienną int
