48efb04db7cbd2aee9a1b4ea9ef90bfb901aef2a
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-07-04 14:43:09 +0200

    Refactor HC-SR04 distance sensor code comments

--- 14_czujnik_odleglosci_hc-sr04/14_czujnik_odleglosci_hc-sr04.ino
+++ 14_czujnik_odleglosci_hc-sr04/14_czujnik_odleglosci_hc-sr04.ino
@@ -3,21 +3,26 @@
 
 void setup() {
   Serial.begin(9600); //Inicjalizacja komunikacji UART
-  pinMode(TrigPin, OUTPUT); //Ustawienie pinu Trig na wyjście
-  pinMode(EchoPin, INPUT); //Ustawienie pinu Echo na wejście 
+  pinMode(TrigPin, OUTPUT); // Ustawienie pinu Trig jako wyjście
+  pinMode(EchoPin, INPUT);  // Ustawienie pinu Echo jako wejście
 }
 
 void loop() {
-  digitalWrite(TrigPin, LOW); //Wykonanie pomiaru
+  // Generowanie impulsu wyzwalającego (Trig)
+  digitalWrite(TrigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(TrigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(TrigPin, LOW);
 
-  long czas = pulseIn(EchoPin, HIGH); //Zmierzenie czasu trwania impulsu na pinie Echo
-  int dystans = czas / 58; //Przekonwertowanie wyniku na centymetry
+  // Pomiar czasu trwania impulsu na pinie Echo
+  long czas = pulseIn(EchoPin, HIGH); 
 
-  Serial.print(dystans); //Wypisanie odległości na komputerze
+  // Przeliczenie czasu na odległość w centymetrach
+  int dystans = czas / 58; 
+  
+  // Wypisanie odległości na komputerze
+  Serial.print(dystans);
   Serial.println(" cm");
   
   delay(500);
